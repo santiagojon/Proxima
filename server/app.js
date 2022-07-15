c384c210ddcc9d5115ba944e8ba9377ecd0e6faa
Chris Zhen <chris.zhenartist@gmail.com>
2022-07-15 09:27:45 -0400

    Revert "Merge pull request #40 from RizzoCapstone/deployment"
    This reverts commit efcac769a3b0961d7d18140265c2e86f297fd8f0, reversing
    changes made to e357cb900803d60b0372f4f3467e13a3322c1f42.

--- server/app.js
+++ server/app.js
@@ -19,11 +19,11 @@ app.use('/api', require('./api'));
 // );
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '..', 'client/build/index.html'))
+  res.sendFile(path.join(__dirname, 'client/build/index.html'))
 );
 
 // static file-serving middleware
-app.use(express.static(path.join(__dirname, '..', 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // any remaining requests with an extension (.js, .css, etc.) send 404
 app.use((req, res, next) => {
@@ -41,7 +41,7 @@ app.use((req, res, next) => {
 // res.sendFile(path.join(__dirname, '..', 'public/index.html'));
 // });
 app.use('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'client/build/index.html'));
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
 // error handling endware
