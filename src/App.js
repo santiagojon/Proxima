e8a010ef5000cb1d7be114c709cf192e9c4dfbb1
ZillaR35 <lonnie.sheppard@rocketmail.com>
2022-07-15 17:07:07 -0400

    merge

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import AppRoutes from './Routes';
-import './App.css';
-import React from 'react';
+import AppRoutes from "./Routes";
+import "./App.css";
+import React from "react";
 // import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       {/* <NavBar /> */}
-      <AppRoutes />
+      <AppRoutes/>
     </div>
   );
 }
