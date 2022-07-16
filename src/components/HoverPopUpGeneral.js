0b0eb9a0941f273cfd26a2fe0d14ccd81fbc3b99
Jon Santiago <jonwsantiago@gmail.com>
2022-07-16 17:12:18 -0400

    Planet data

--- src/components/HoverPopUpGeneral.js
+++ src/components/HoverPopUpGeneral.js
@@ -14,16 +14,29 @@ const HoverPopUpGeneral = (props) => {
       return (
         <div className="popupRenderTextPlanetInfo">
           <div>
-            As many as four of the planets (d, e, f, g) are hypothesised to be
-            orbiting within the habitable zone of the star and thus to have
-            temperatures suitable to the presence of liquid water and the
-            development of life. Whether they actually contain liquid water is a
-            function of numerous properties of the planets including whether
-            they have an atmosphere. There is no clear evidence that any of the
-            planets has an atmosphere and it is unclear whether planets could
-            retain an atmosphere given TRAPPIST-1's radiation emission. The low
-            densities of the planets indicate they may consist of large amounts
-            of volatile material.
+            <div>
+              As many as four of the planets (d, e, f, g) in TRAPPIST-1 are
+              hypothesised to be orbiting within the habitable zone of the star
+              and thus to have temperatures suitable to the presence of liquid
+              water and the development of life.
+            </div>
+            <br></br>
+            <div>
+              Whether they actually contain liquid water is a function of
+              numerous properties of the planets including whether they have an
+              atmosphere. There is no clear evidence that any of the planets has
+              an atmosphere and it is unclear whether planets could retain an
+              atmosphere given TRAPPIST-1's radiation emission.
+              <br></br>
+              <br></br>
+              <div>
+                NASA's new James Webb Telescope will spend a quarter of its
+                first year observing exoplanets, and 8.2% of this time studying
+                TRAPPIST-1. Its infrared cameras will give us a better
+                understanding of whether or not atmospheres exist on these
+                exoplanets.
+              </div>
+            </div>
           </div>
           <div></div>
         </div>
@@ -34,11 +47,12 @@ const HoverPopUpGeneral = (props) => {
       return (
         <div className="popupRenderTextPlanetInfo">
           <div>
-            At only four light-years away, Proxima Centauri-b is our closest
-            known exoplanet neighbor. Proxima b is a super Earth exoplanet that
-            orbits a M-type star. Its mass is 1.27 Earths, it takes 11.2 days to
-            complete one orbit of its star, and is 0.0485 AU from its star. Its
-            discovery was announced in 2016.{" "}
+            At only four light-years away, Proxima Centauri is our closest known
+            exoplanet system neighbor. The only confirmed planet in this system,
+            Proxima b is a super Earth exoplanet that orbits a M-type star. Its
+            mass is 1.27 Earths, it takes 11.2 days to complete one orbit of its
+            star, and is 0.0485 AU from its star. Its discovery was announced in
+            2016.
           </div>
           <br></br>
           <div>
--- src/components/SolarSystemViewInfo.js
+++ src/components/SolarSystemViewInfo.js
@@ -1,92 +1,93 @@
-import React from 'react';
-import HoverPopUpGeneral from './HoverPopUpGeneral';
+import React from "react";
+import HoverPopUpGeneral from "./HoverPopUpGeneral";
 
 const SolarSystemViewInfo = (props) => {
+  const solarSystem = props.solarSystem;
+  console.log("SSProps", props);
 
-    const solarSystem = props.solarSystem;
-    console.log('SSProps', props)
+  //Featured star systems w/ additional information
+  if (
+    solarSystem &&
+    solarSystem.length &&
+    solarSystem[0].name === "TRAPPIST-1"
+  ) {
+    return (
+      <div>
+        <div className="planetText_Name">
+          {solarSystem && solarSystem.length ? solarSystem[0].name : ""}
+        </div>
+        <div className="planetText_Type">Ultra Cool Red-Dwarf Star</div>
+        <div className="starText_Age">7.6 billion years old</div>
+        <div className="starText_Habitable">
+          <HoverPopUpGeneral solarSystem={solarSystem} />
+        </div>
+      </div>
+    );
+  }
 
-    //Featured star systems w/ additional information 
-    if(solarSystem && solarSystem.length && solarSystem[0].name === 'TRAPPIST-1') {
-        return (
-        <div>
-            <div className="planetText_Name">
-                {solarSystem && solarSystem.length ? solarSystem[0].name : ''}
-            </div>
-            <div className="planetText_Type">
-               Ultra Cool Red-Dwarf Star
-            </div>
-            <div className="starText_Age">
-               7.6 billion years old
-            </div>
-            <div className="starText_Habitable">
-               <HoverPopUpGeneral solarSystem={solarSystem}/>
-            </div>
-        </div>)
-    }
+  if (
+    solarSystem &&
+    solarSystem.length &&
+    solarSystem[0].name === "Proxima Cen"
+  ) {
+    return (
+      <div>
+        <div className="planetText_Name">Proxima Centauri</div>
+        <div className="planetText_Type">Red-Dwarf Star</div>
+        <div className="starText_Age">4.9 billion years old</div>
+        <div className="starText_Habitable">
+          <HoverPopUpGeneral solarSystem={solarSystem} />
+        </div>
+      </div>
+    );
+  }
+  //**********************************************************************
 
-    if(solarSystem && solarSystem.length && solarSystem[0].name === 'Proxima Cen') {
-        return (<div>
-              <div className="planetText_Name">
-                Proxima Centauri
-            </div>
-            <div className="planetText_Type">
-               Red-Dwarf Star
-            </div>
-            <div className="starText_Age">
-               4.9 billion years old
-            </div>
-            <div className="starText_Habitable">
-               <HoverPopUpGeneral solarSystem={solarSystem}/>
-            </div>
-        </div>)
-    }
-    //**********************************************************************
-
-
-    // <div className="starText_Habitable">
-    // {solarSystem && solarSystem.length  ? "Habitable Planets: 1": ''} 
-    // </div>
-
-    return(
+  return (
     <div>
-    <div className="planetText_Name">
-    {solarSystem && solarSystem.length ? solarSystem[0].name : ''}
+      <div className="planetText_Name">
+        {solarSystem && solarSystem.length ? solarSystem[0].name : ""}
+      </div>
+      <div className="planetText_Type">
+        {solarSystem && solarSystem.length && solarSystem[0].starSpectralType
+          ? `Star Type: ${solarSystem[0].starSpectralType}`
+          : ""}
+      </div>
+      <div className="planetText_Type">
+        {solarSystem && solarSystem.length && solarSystem[0].name === "Sun"
+          ? "Yellow Dwarf"
+          : ""}
+      </div>
+      <div className="starText_Age">
+        {solarSystem &&
+        solarSystem.length &&
+        solarSystem[0].starAge &&
+        solarSystem[0].starAge !== 0
+          ? ` ${Math.round(solarSystem[0].starAge * 10) / 10} billion years old`
+          : ""}
+      </div>
     </div>
-    <div className="planetText_Type">
-    {solarSystem && solarSystem.length && solarSystem[0].starSpectralType ? `Star Type: ${solarSystem[0].starSpectralType}`: ''} 
-    </div>
-    <div className="starText_Age">
-    {solarSystem && solarSystem.length  && solarSystem[0].starAge !== null ? ` ${(Math.round(solarSystem[0].starAge * 10) / 10)} billion years old` : ''} 
-    </div>
-    
-    </div>
-    )
-}
-
+  );
+};
 
 export default SolarSystemViewInfo;
-
-
-
-
 
 //Saturday
 
 //Star System View
-    //**************************************Add starSpectralType property to data parser
-        //**************************************Add this underneath star name
-        //**************************************perhaps another piece of data about the star
-        //distance too??? (At least for the featured ones)
+//**************************************Add starSpectralType property to data parser
+//**************************************Add this underneath star name
+//**************************************perhaps another piece of data about the star
+//distance too??? (At least for the featured ones)
 
 //Featured Single Planets in Trappist-1, Proxima, etc.
-    //Add additional info for those planets. Just one or two lines
+//Add additional info for those planets. Just one or two lines
 
 //Planet rings color change broken for some reason. Fix with Chris
 
 //Planet mesh shader change on planet selection. Do this with Chris.
 
 //Styling
-    //Milky Way
-    //Info page + images
-    //Anywhere else that needs it
+//Milky Way
+//Info page + images
+//Anywhere else that needs it
